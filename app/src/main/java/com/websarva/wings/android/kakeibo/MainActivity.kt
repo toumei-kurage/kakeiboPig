24595f194602d9adb136760a61fcb403fb8b7dd6
toumei-kurage <yoshikawa60065@gmail.com>
2024-09-26 19:58:30 +0900

    メイン画面にアクションバー追加

--- app/src/main/java/com/websarva/wings/android/kakeibo/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/kakeibo/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.websarva.wings.android.kakeibo
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -10,5 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Toolbarを設定
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.setTitle(R.string.app_name)
+        toolbar.setTitleTextColor(Color.WHITE)
+        setSupportActionBar(toolbar)
     }
 }
