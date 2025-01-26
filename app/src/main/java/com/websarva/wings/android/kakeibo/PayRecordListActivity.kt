5c62aeb65fd15c354fe071179d75196c1008cc53
toumei-kurage <yoshikawa60065@gmail.com>
2025-01-26 18:12:37 +0900

    読み込みメソッドの変更

--- app/src/main/java/com/websarva/wings/android/kakeibo/PayRecordListActivity.kt
+++ app/src/main/java/com/websarva/wings/android/kakeibo/PayRecordListActivity.kt
@@ -39,7 +39,7 @@ class PayRecordListActivity : BaseActivity(R.layout.activity_pay_record_list, R.
         payRecordAdapter = PayRecordAdapter(this,payRecordList)
         recyclerView.adapter = payRecordAdapter
 
-        loadPayRecordList()
+        applyRefinement(null,null,null)
 
         buttonPayRecordAdd.setOnClickListener {
             val intent = Intent(this, PayRecordAddActivity::class.java)
@@ -54,37 +54,6 @@ class PayRecordListActivity : BaseActivity(R.layout.activity_pay_record_list, R.
         }
     }
 
-    @SuppressLint("Range", "NotifyDataSetChanged")
-    private fun loadPayRecordList() {
-        val db = DatabaseHelper(this).readableDatabase
-        val cursor: Cursor = db.rawQuery("SELECT * FROM payment_history WHERE user_id = ?", arrayOf(userID))
-        val newPayRecordList = mutableListOf<PayRecord>()
-        if (cursor.moveToFirst()) {
-            do {
-                val id = cursor.getLong(cursor.getColumnIndex("_id"))
-                val userId = cursor.getString(cursor.getColumnIndex("user_id"))
-                val memberId = cursor.getLong(cursor.getColumnIndex("member_id"))
-                val payPurposeId = cursor.getLong(cursor.getColumnIndex("purpose_id"))
-                val payDate = cursor.getString(cursor.getColumnIndex("payment_date"))
-                val payAmount = cursor.getInt(cursor.getColumnIndex("amount"))
-                val isReceptChecked = cursor.getInt(cursor.getColumnIndex("is_recept_checked")) == 1
-                val note = cursor.getString(cursor.getColumnIndex("note"))
-                newPayRecordList.add(PayRecord(id, userId, memberId,payPurposeId,payDate,payAmount,isReceptChecked,note))
-
-            } while (cursor.moveToNext())
-        } else {
-            Toast.makeText(this, "支払い履歴が登録されていません。", Toast.LENGTH_SHORT).show()
-        }
-
-        cursor.close()
-        db.close()
-
-        // データセットを更新
-        payRecordList = newPayRecordList
-        payRecordAdapter.updateData(payRecordList)
-        payRecordAdapter.notifyDataSetChanged()
-    }
-
     // onActivityResultをオーバーライドして削除結果を受け取る
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -93,7 +62,7 @@ class PayRecordListActivity : BaseActivity(R.layout.activity_pay_record_list, R.
             // 削除された場合の処理
             if (data?.getBooleanExtra("PAY_RECORD_DELETE", false) == true) {
                 // 削除後にデータを再読み込みしてRecyclerViewを更新
-                loadPayRecordList()
+                applyRefinement(null,null,null)
             }
         }
     }
