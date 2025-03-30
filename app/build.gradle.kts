a44822c52251b0dcfe6bf08d9f53576cf19a25ca
toumei-kurage <yoshikawa60065@gmail.com>
2025-03-30 14:22:07 +0900

    バージョンコード4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.websarva.wings.android.kakeibo0422"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
