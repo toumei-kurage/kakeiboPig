6aac63478e51c76dbebf939e8c26d1df4c207ce2
yoshikawa <yoshikawa60065@gmail.com>
2025-09-14 19:06:56 +0900

    バージョンコード7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.websarva.wings.android.kakeibo0422"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.2"
+        versionCode = 7
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
